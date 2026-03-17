d407d75ca46a8fe18dc701dc5f0e3642be114c81
Noel Moreno Garcia <theinhumanride10@gmail.com>
2026-03-17 22:45:27 +0000

    pt2

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  // Eliminamos output: 'export' para permitir Server Actions
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
